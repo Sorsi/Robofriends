12feb99c4889869c662f839b1797dd7a12ee7f7e
Sorsi <orsolya.suto.so@gmail.com>
2018-10-15 15:24:57 +0200

    make card component and create mock datas for robots

---
+++ robofriends/src/Robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Edi Bereczki',
+    username: 'BEdi',
+    email: 'beedi@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Orsi Suto',
+    username: 'SOrsi',
+    email: 'soorsi@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Eszti Dalos',
+    username: 'Csita',
+    email: 'csita@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Barni Papp',
+    username: 'Parni',
+    email: 'parni@gmail.com'
+  },
+]
