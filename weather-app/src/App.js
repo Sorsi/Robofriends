ca2c3e558231bbe4f04e1ff7fdb595ac7eca0aca
Sorsi <orsolya.suto.so@gmail.com>
2018-10-17 02:10:59 +0200

    define a state

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -6,10 +6,18 @@ import Weather from './components/Weather';
 const API_KEY = 'bea5333b320643bb4ca57cd6ed1aabf4';
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
   getWeather = async (e) => {
     e.preventDefault();
-    const city = e.target.elements.name.city.value;
-    const country = e.target.elements.name.country.value;
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();        
   }
