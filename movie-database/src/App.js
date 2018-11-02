071f7d51e70f1326372add1877485540bf1a6911
Sorsi <orsolya.suto.so@gmail.com>
2018-11-02 14:05:39 +0100

    play with a mock object, render datas from state

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
-  state={
-    rows: [
-    <p key="1">This is my row</p>,
-    <p key="2">This is my row</p>,
-    <p key="3">This is my row</p>,
+  constructor(props) {
+    super(props)
+    console.log('This is my init')
+
+    const movies = [
+      { id: 0, title: 'Awangers', overview: 'blalkdsfklfdkfdklfdklkldfslkdf' },
+      { id: 1, title: 'Superman', overview: 'blalkdsfklfdkfdklfdklkldfslkdf' },
+      { id: 2, title: 'Batman', overview: 'blalkdsfklfdkfdklfdklkldfslkdf' },
     ]
+
+    this.state = {
+      rows: [
+        <p key="1">This is my row 1</p>,
+        <p key="2">This is my row 2</p>,
+        <p key="3">This is my row 3</p>,
+      ]
+    }
+
+    let movieRows = []
+
+    movies.forEach((movie) => {
+      console.log(movie.title)
+      movieRows.push(<p> movie title: {movie.title}</p>)
+    });
+
+    this.state = {rows: movieRows}
   }
 
   render() {
@@ -32,7 +54,7 @@ class App extends Component {
           placeholder="Search here"
           type="text"
         />
-      {this.state.rows}
+        {this.state.rows}
       </div>
     );
   }
