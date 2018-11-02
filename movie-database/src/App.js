cee9351da64f40e831ff2d4816b88601891a31e5
Sorsi <orsolya.suto.so@gmail.com>
2018-11-02 16:07:56 +0100

    get back datas from movie DB API

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieRow from './MovieRow';
-
+import $ from 'jquery';
 
 
 class App extends Component {
   constructor(props) {
     super(props)
-    console.log('This is my init')
+    this.state = {}
 
-    const movies = [
+    /* const movies = [
       { id: 0, poster_src: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/1ZEJuuDh0Zpi5ELM3Zev0GBhQ3R.jpg', title: 'Awangers', overview: 'blalkdsfklfdkfdklfdklkldfslkdf' },
       { id: 1, poster_src: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/7Gfda4S0FpWf6edfGFm0xJgRJ4b.jpg', title: 'Superman', overview: 'blalkdsfklfdkfdklfdklkldfslkdf' },
       { id: 2, poster_src: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/dnLvZJD8Zdw2fHzL4AtM0kZSkt2.jpg', title: 'Batman', overview: 'blalkdsfklfdkfdklfdklkldfslkdf' },
@@ -22,7 +22,35 @@ class App extends Component {
       movieRows.push(movieRow)
     });
 
-    this.state = {rows: movieRows}
+    this.state = {rows: movieRows} */
+
+    this.performSearch()
+  }
+
+  performSearch() {
+    console.log('Search using Movie DB')
+    const urlString = 'https://api.themoviedb.org/3/search/movie?query=DC&api_key=896216e67d817174defc8cd1d41bd353'
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log('Fetched data successfully')
+        console.log(searchResults)
+        const results = searchResults.results
+
+        let movieRows = []
+
+        results.forEach((movie) => {
+          console.log(movie.poster_path)
+          const movieRow = <MovieRow movie={movie}/>
+          movieRows.push(movieRow)
+        });
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.log('Failed to fetch data')
+      }
+    })
   }
 
   render() {
