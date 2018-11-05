26afced57c82b0a091051f2087059537056daf05
Sorsi <orsolya.suto.so@gmail.com>
2018-11-05 19:13:36 +0100

    click view button and go to the original website with the choosen movie's details

--- movie-database/src/MovieRow.js
+++ movie-database/src/MovieRow.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class MovieRow extends React.Component {
+
+  viewMovie() {
+    /* console.log('kljsdfklsdkljdsf')
+    console.log(this.props.movie.title) */
+    window.location.href = `https://www.themoviedb.org/movie/${this.props.movie.id}`
+  }
+
   render() {
     return <div>
       <table key={this.props.movie.id}>
@@ -10,8 +17,9 @@ class MovieRow extends React.Component {
               <img src={this.props.movie.poster_src} alt="poster" width="120" />
             </td>
             <td>
-              {this.props.movie.title}
+              <h3>{this.props.movie.title}</h3>
               <p>{this.props.movie.overview}</p>
+              <input type="button" onClick={this.viewMovie.bind(this)} value="view" />
             </td>
           </tr>
         </tbody>
