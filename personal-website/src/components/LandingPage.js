694e9151a5f7c26576c167be3db415a0eba0590d
Sorsi <orsolya.suto.so@gmail.com>
2018-11-06 11:27:01 +0100

    modify icons and links

--- personal-website/src/components/LandingPage.js
+++ personal-website/src/components/LandingPage.js
@@ -17,6 +17,17 @@ class Landing extends Component {
               <h1>Junior Sorftware Developer</h1>
               <hr />
               <p>HTML/CSS | Ant Design | JavaScript | Raact | Redux | MongoDB</p>
+              <div className="social-links">
+                <a href="https://www.linkedin.com/in/orsolya-suto/" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                <a href="https://github.com/Sorsi" target="_blank">
+                  <i className="fa fa-github-square" />
+                </a>
+                <a href="https://www.freecodecamp.org/sorsi" target="_blank">
+                  <i className="fa fa-free-code-camp" />
+                </a>
+              </div>
             </div>
           </Cell>
         </Grid>
