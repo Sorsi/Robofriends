43afe1ce562ec95826b43d4e15eb6658a04a47c5
Sorsi <orsolya.suto.so@gmail.com>
2018-11-06 13:56:10 +0100

    use google font Anton, separate page's CSS, use Grid and Cell create contact page

--- personal-website/src/components/Contact.js
+++ personal-website/src/components/Contact.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
 
 class Contact extends Component {
   render() {
-    return(
-      <div>
-        <h1>
-          Contact Page
-        </h1>
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>LEFT</Cell>
+          <Cell col={6}>RIGHT</Cell>
+        </Grid>
       </div>
-    )
-  }
-}
-
-export default Contact;
+        )
+      }
+    }
+    
+    export default Contact;
